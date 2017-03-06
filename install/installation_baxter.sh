5a523160dee6603e5d35179b37e1e7c25bd07ce4
sallath <sallath@live.com>
2017-03-07 00:21:47 +0100

    minnor script changes

--- install/installation_baxter.sh
+++ install/installation_baxter.sh
@@ -4,6 +4,9 @@ IFS=$'\n\t'
 
 export DEBIAN_FRONTEND=noninteractive
 
+# 1 # --- --- Install Ubuntu 14.05 --- --- #
+
+# 2 # --- --- Install ROS --- --- #
 # Install ROS Indigo
 ## Setup your sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -19,29 +22,68 @@ rosdep update
 ## Install rosinstall
 sudo apt-get install -y python-rosinstall
 
-# Create Baxter Development Workspace
-## Create ROS Workspace
+
+# 3 # --- --- Create Baxter Development Workspace --- --- #
+# Create ROS Workspace
 mkdir -p ~/ros_ws/src
+# Source ROS and Build
 ## Source ROS Setup
 source /opt/ros/indigo/setup.bash
+## Build and Install
 cd ~/ros_ws
 catkin_make
 catkin_make install
 
-# Install Baxter SDK Dependencies
+
+# 4 # --- --- Install Baxter SDK Dependencies --- --- #
+# Install SDK Dependencies
 sudo apt-get update
 sudo apt-get install -y git-core python-argparse python-wstool python-vcstools python-rosdep ros-indigo-control-msgs ros-indigo-joystick-drivers
 
-# Install Baxter Research Robot SDK
-## Install Baxter SDK
+
+# 5 # --- --- Install Baxter Research Robot SDK --- --- #
+# Install Baxter SDK
 cd ~/ros_ws/src
 wstool init .
 wstool merge https://raw.githubusercontent.com/RethinkRobotics/baxter/master/baxter_sdk.rosinstall
 wstool update
-## Source ROS Setup 
+# Source ROS Setup 
 source /opt/ros/indigo/setup.bash
-## Build and Install
+# Build and Install
 cd ~/ros_ws
 catkin_make
 catkin_make install
 
+# 6 # --- --- Configure Baxter Communication/ROS Workspace --- --- #
+# Download the baxter.sh script
+wget https://github.com/RethinkRobotics/baxter/raw/master/baxter.sh
+chmod u+x baxter.sh
+
+# Customize the baxter.sh script
+## Edit the 'baxter_hostname' field
+### Baxter's hostname is defaulted as the robot's serial number. The serial number can be located on the back of the robot, next to the power button.
+###  Specify Baxter's hostname
+### **baxter_hostname="baxter_hostname.local"**
+
+## Edit the 'your_ip' field
+### Modify where 'your_ip' is the IP address of your PC.
+### **your_ip="192.168.XXX.XXX"**
+
+## Verify 'ros_version' field
+### Verify that the the 'ros_version' field matches the ROS version you are running:
+### This field will default to "indigo"
+### ***ros_version="indigo"***
+
+## Save and Close baxter.sh script
+
+## Initialize your SDK environment
+### cd ~/ros_ws
+### . baxter.sh
+
+# 7 # --- --- Verify Environment --- --- #
+env | grep ROS
+### The important fields at this point:
+### ROS_MASTER_URI - This should now contain your robot's hostname.
+### ROS_IP - This should contain your workstation's IP address.
+### or
+### ROS_HOSTNAME - If not using the workstation's IP address, the ROS_HOSTNAME field should contain your PC's hostname. Otherwise, this field should not be available. 
